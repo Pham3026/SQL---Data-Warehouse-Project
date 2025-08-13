f2c87a357a86483ab82c989a5b4a184ba06f09fd
Oanh Pham Thi <phamthioanh302@gmail.com>
2025-08-13 10:51:11 +0700

    Sliver_ddl & proc load.sql

--- Script/Sliver_ddl & proc load.sql
+++ Script/Sliver_ddl & proc load.sql
@@ -1,1 +1,9 @@
-
+/*
+===============================================================================
+1. DDL Script: Create Silver Tables
+2. Stored Procedure: Load Silver Layer (Bronze -> Silver)
+  Script Purpose:
+    This stored procedure performs the ETL (Extract, Transform, Load) process to 
+    populate the 'silver' schema tables from the 'bronze' schema.
+===============================================================================
+*/
